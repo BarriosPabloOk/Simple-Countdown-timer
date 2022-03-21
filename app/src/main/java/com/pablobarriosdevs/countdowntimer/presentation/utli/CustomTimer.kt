68fe102d49ff51ef68c502a6bb3e0905701f727d
Pablo Barrios <pablob1518@hotmail.com>
2022-03-21 19:23:05 -0300

    last commit

--- app/src/main/java/com/pablobarriosdevs/countdowntimer/presentation/utli/CustomTimer.kt
+++ app/src/main/java/com/pablobarriosdevs/countdowntimer/presentation/utli/CustomTimer.kt
@@ -3,6 +3,12 @@ package com.pablobarriosdevs.countdowntimer.presentation.utli
 import android.os.CountDownTimer
 import androidx.compose.runtime.MutableState
 
+
+/*
+*This class is ready to be used in Jetpack compose.
+*The reason for this is to be able to use it in projects
+*where the timer is a secondary feature.
+*/
 class CustomTimer(
     var initTime: MutableState<Long>,
     val interval: Long = 1000L,
